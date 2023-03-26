0cbb3727dac137f01e522525bc9eaa5bbfab0194
Joel B. Mohler <joel@kiwistrawberry.us>
2023-03-26 02:24:28 -0400

    some triangle inclusion

--- engine.js
+++ engine.js
@@ -1,6 +1,108 @@
 // Get the canvas and context
 const canvas = document.getElementById("canvas");
 
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+  }
+
+  subtract(other) {
+    return new Vector(this.x - other.x, this.y - other.y);
+  }
+
+  scale(factor) {
+    return new Vector(factor * this.x, factor * this.y);
+  }
+
+  dot(other) {
+    return this.x * other.x + this.y * other.y;
+  }
+}
+
+function isPointInTriangle(p, a, b, c) {
+  p = new Vector(p.x, p.y);
+  a = new Vector(a.x, a.y);
+  b = new Vector(b.x, b.y);
+  c = new Vector(c.x, c.y);
+
+  const v0 = c.subtract(a);
+  const v1 = b.subtract(a);
+  const v2 = p.subtract(a);
+
+  const dot00 = v0.dot(v0);
+  const dot01 = v0.dot(v1);
+  const dot02 = v0.dot(v2);
+  const dot11 = v1.dot(v1);
+  const dot12 = v1.dot(v2);
+
+  const invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+  const u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+  const v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+  return u >= 0 && v >= 0 && u + v < 1;
+}
+
+class Polygon {
+  constructor(center, sides, radius, rotation) {
+    this.center = center;
+    this.sides = sides;
+    this.radius = radius;
+    this.rotation = rotation;
+
+    this._vertices = null;
+  }
+
+  vertices() {
+    if (this._vertices === null) {
+      const angle = (Math.PI * 2) / this.sides;
+      this._vertices = [];
+      for (let i = 0; i < this.sides; i++) {
+        const x =
+          this.center.x + this.radius * Math.cos(this.rotation + angle * i);
+        const y =
+          this.center.y + this.radius * Math.sin(this.rotation + angle * i);
+
+        this._vertices.push({ x, y });
+      }
+    }
+
+    return this._vertices;
+  }
+}
+
+function iterateCircularPairs(array) {
+  const n = array.length;
+  const pairs = [];
+
+  for (let i = 0; i < n; i++) {
+    const j = (i + 1) % n;
+    pairs.push([array[i], array[j]]);
+  }
+
+  return pairs;
+}
+
+function s(v) {
+  return `(${v.x.toFixed(1)}, ${v.y.toFixed(1)})`;
+}
+
+function isInterior(p1, p2) {
+  for (let ip2 of p2.vertices()) {
+    for (const [v1, v2] of iterateCircularPairs(p1.vertices())) {
+      console.log(`Testing ${s(ip2)} is in side ${s(v1)}, ${s(v2)}`);
+
+      if (isPointInTriangle(ip2, p1.center, v1, v2)) {
+        console.log(`Point ${s(ip2)} is in side ${s(v1)}, ${s(v2)}`);
+      }
+    }
+  }
+}
+
 // Define a function to generate a regular polygon with a given center and radius
 function generateRegularPolygon(
   center,
@@ -47,19 +149,6 @@ function generateRegularPolygon(
   ctx.stroke();
 }
 
-class Polygon {
-  constructor(center, sides, radius, strokeColor) {
-    this.center = center;
-    this.sides = sides;
-    this.radius = radius;
-    this.strokeColor = strokeColor;
-
-    this._vertices = null;
-  }
-
-  computeVertices() {}
-}
-
 function generatePolygon() {
   // Get the side count dropdown and generate button
   const sideCountSelect = document.getElementById("sideCount");
@@ -78,4 +167,9 @@ function generatePolygon() {
 
   // Call the generateRegularPolygon function to draw the polygon
   generateRegularPolygon(center, sides, radius, Math.PI / 3, "blue");
+
+  const p1 = new Polygon(center, 4, 5, Math.PI / 3);
+  const p2 = new Polygon(center, 3, 10, Math.PI / 2.95);
+
+  isInterior(p2, p1);
 }
